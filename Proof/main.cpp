941723712470b596e61fe474c76245a88223938c
ISHII Tsuubito <ishii@buffer-renaiss.com>
2014-12-30 15:09:29 +0900

    Initial Commit

---
+++ Proof/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proof
+//
+//  Created by ISHII 2bit on 2014/12/30.
+//  Copyright (c) 2014年 bufferRenaiss. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
