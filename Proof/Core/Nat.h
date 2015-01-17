3ba896467ce2f591c769dc4fe6bfd44ac50633ed
ISHII Tsuubito <ishii@buffer-renaiss.com>
2015-01-18 03:23:32 +0900

    add constraints about types.

--- Proof/Core/Nat.h
+++ Proof/Core/Nat.h
@@ -20,62 +20,111 @@ namespace Nat {
     template <typename ... Ms>
     using AreNats = Reduce<Meta::And, True, Map<IsNat, Set<Ms ...>>>;
     
+    /**
+     *  Zero
+     *  @return 0
+     */
     struct Zero : public NatType {};
     
-    template <typename M, typename = Enable<AreNats<M>>>
-    struct Suc : public NatType {};
+    /**
+     *  Succesor
+     *
+     *  @param M <b>requires</b> NatType
+     *
+     *  @return Suc(M) Succesor of M
+     */
+    template <typename M, requires(AreNats<M>)>
+    struct Suc : public Requires(NatType) {};
     
-    template <typename M, typename N, typename = Enable<AreNats<M, N>>>
-    struct Add : public NatType {};
+    /**
+     *  Addition of Nat
+     *
+     *  @param M <b>requires</b> NatType
+     *  @param N <b>requires</b> NatType
+     *
+     *  @return M+N NatType
+     */
+    template <typename M, typename N, requires(AreNats<M, N>)>
+    struct Add : public Requires(NatType) {};
     
-    template <typename M, typename N, typename = Enable<AreNats<M, N>>>
-    struct Mul : public NatType {};
+    /**
+     *  Multiply of Nat
+     *
+     *  @param M <b>requires</b> NatType
+     *  @param N <b>requires</b> NatType
+     *
+     *  @return M*N NatType
+     */
+    template <typename M, typename N, requires(AreNats<M, N>)>
+    struct Mul : public Requires(NatType) {};
 };
 
 namespace Axiom {
+    using namespace Nat;
     class PA {
     public:
-        // m = n -> s(m) = s(n)
-        template <typename M, typename N, typename ... As, typename = Enable<Nat::AreNats<M, N>>>
+        /**
+         *  m = n -> s(m) = s(n)
+         *
+         *  @param M <b>requires</b> NatType
+         *  @param N <b>requires</b> NatType
+         *  @param As... Array of Propositions <b>requires</b> PropositionType
+         *  @param Formula<Eq<M,N>,Assumptions<As...>>
+         *
+         *  @return Formula<Eq<Nat::Suc<M>,Nat::Suc<N>>,Assumptions<As...>>
+         */
+        template <typename M, typename N, typename ... As, requires(Meta::And<AreNats<M, N>, AreProps<As ...>>)>
         static auto eq_suc(const Formula<Eq<M, N>, Assumptions<As ...>> &)
-        -> Formula<Eq<Nat::Suc<M>, Nat::Suc<N>>, Assumptions<As ...>> {
+        -> Formula<Eq<Nat::Suc<M>, Nat::Suc<N>>, Assumptions<As ...>>
+        {
+            fulfill(AreNats<M, N>);
             return Formula<Eq<Nat::Suc<M>, Nat::Suc<N>>, Assumptions<As ...>>();
         }
         
         // add(m, 0) = m
-        template <typename M, typename = Enable<Nat::AreNats<M>>>
+        template <typename M, requires(AreNats<M>)>
         static auto add_m_0_is_m()
-        -> Formula<Eq<Nat::Add<M, Nat::Zero>, M>, Assumptions<>> {
+        -> Formula<Eq<Nat::Add<M, Nat::Zero>, M>, Assumptions<>>
+        {
+            fulfill(AreNats<M>);
             return Formula<Eq<Nat::Add<M, Nat::Zero>, M>, Assumptions<>>();
         }
 
         // add(m, s(n)) = s(add(m, n))
-        template <typename M, typename N, typename = Enable<Nat::AreNats<M, N>>>
+        template <typename M, typename N, requires(AreNats<M, N>)>
         static auto add_m_sn_is_s_add_m_n()
-        -> Formula<Eq<Nat::Add<M, Nat::Suc<N>>, Nat::Suc<Nat::Add<M, N>>>, Assumptions<>> {
+        -> Formula<Eq<Nat::Add<M, Nat::Suc<N>>, Nat::Suc<Nat::Add<M, N>>>, Assumptions<>>
+        {
+            fulfill(AreNats<M, N>);
             return Formula<Eq<Nat::Add<M, Nat::Suc<N>>, Nat::Suc<Nat::Add<M, N>>>, Assumptions<>>();
         }
 
         // mul(m, 0) = 0
-        template <typename M, typename = Enable<Nat::AreNats<M>>>
+        template <typename M, requires(AreNats<M>)>
         static auto mul_m_0_is_0()
-        -> Formula<Eq<Nat::Mul<M, Nat::Zero>, Nat::Zero>, Assumptions<>> {
+        -> Formula<Eq<Nat::Mul<M, Nat::Zero>, Nat::Zero>, Assumptions<>>
+        {
+            fulfill(AreNats<M>);
             return Formula<Eq<Nat::Mul<M, Nat::Zero>, Nat::Zero>, Assumptions<>>();
         }
         
         // mul(m, s(n)) = add(m, mul(m, n))
-        template <typename M, typename N, typename = Enable<Nat::AreNats<M, N>>>
+        template <typename M, typename N, requires(AreNats<M, N>)>
         static auto mul_m_sn_is_add_m_mul_m_n()
-        -> Formula<Eq<Nat::Mul<M, Nat::Suc<N>>, Nat::Add<M, Nat::Mul<M, N>>>, Assumptions<>> {
+        -> Formula<Eq<Nat::Mul<M, Nat::Suc<N>>, Nat::Add<M, Nat::Mul<M, N>>>, Assumptions<>>
+        {
+            fulfill(AreNats<M, N>);
             return Formula<Eq<Nat::Mul<M, Nat::Suc<N>>, Nat::Add<M, Nat::Mul<M, N>>>, Assumptions<>>();
         }
         
         // induction
         // P(0) => P(m) => P(s(m)) => ∀m.P(m)
-        template <typename X, typename Prop, typename K, typename ... As, typename ... Bs, typename = Enable<Nat::AreNats<X, K>>>
+        template <typename X, typename Prop, typename K, typename ... As, typename ... Bs, requires(Meta::And<AreNats<X, K>, AreProps<Prop, As ..., Bs ...>>)>
         static auto induction(const Formula<Substitute<Prop, X, Nat::Zero>, Assumptions<As ...>> &,
                               const Formula<Imp<Substitute<Prop, X, K>, Substitute<Prop, X, Nat::Suc<K>>>, Assumptions<Bs ...>> &)
-        -> Formula<ForAll<X, Prop>, MakeAssumptions<As ..., Bs ...>> {
+        -> Formula<ForAll<X, Prop>, MakeAssumptions<As ..., Bs ...>>
+        {
+            fulfill(AreNats<X, K>, AreProps<Prop, As ..., Bs ...>);
             return Formula<ForAll<X, Prop>, MakeAssumptions<As ..., Bs ...>>();
         }
     };
