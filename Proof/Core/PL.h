77c64aae3fc0fb69f71c7662c2e87e4e194ccded
ISHII Tsuubito <ishii@buffer-renaiss.com>
2015-01-18 17:14:28 +0900

    型チェックを無駄にやり過ぎてたので修正

--- Proof/Core/PL.h
+++ Proof/Core/PL.h
@@ -31,97 +31,97 @@ namespace Axiom {
         }
         
         // P, Gamma |- F --> Gamma |- -P
-        template <typename P, typename ... Gamma, requires(AreProps<P, Gamma ...>)>
+        template <typename P, typename ... Gamma, requires(AreProps<P>)>
         static auto notI(const Formula<Contradiction, Assumptions<P, Gamma ...>> &)
         -> Formula<Not<P>, Assumptions<Gamma ...>>
         {
-            fulfill(AreProps<P, Gamma ...>);
+            fulfill(AreProps<P>);
             return Formula<Not<P>, Assumptions<Gamma ...>>();
         }
         
         // Gamma |- P + Delta |- -P --> Gamma, Delta |- Contradiction
-        template <typename P, typename ... Gamma, typename ... Delta, requires(AreProps<P, Gamma ..., Delta ...>)>
+        template <typename P, typename ... Gamma, typename ... Delta, requires(AreProps<P>)>
         static auto notE(const Formula<P, Assumptions<Gamma ...>> &,
                          const Formula<Not<P>, Assumptions<Delta ...>> &)
         -> Formula<Contradiction, MakeAssumptions<Gamma ..., Delta ...>>
         {
-            fulfill(AreProps<P, Gamma ..., Delta ...>);
+            fulfill(AreProps<P>);
             return Formula<Contradiction, MakeAssumptions<Gamma ..., Delta ...>>();
         }
         
         // Gamma |- P + Delta |- Q --> Gamma, Delta |- P /\ Q
-        template <typename P, typename Q, typename ... Gamma, typename ... Delta, requires(AreProps<P, Q, Gamma ..., Delta ...>)>
+        template <typename P, typename Q, typename ... Gamma, typename ... Delta, requires(AreProps<P, Q>)>
         static auto andI(const Formula<P, Assumptions<Gamma ...>> &proof1,
                          const Formula<Q, Assumptions<Delta ...>> &proof2)
         -> Formula<And<P, Q>, MakeAssumptions<Gamma ..., Delta ...>>
         {
-            fulfill(AreProps<P, Q, Gamma ..., Delta ...>);
+            fulfill(AreProps<P, Q>);
             return Formula<And<P, Q>, MakeAssumptions<Gamma ..., Delta ...>>();
         }
         
         // Gamma |- P /\ Q --> Gamma |- P
-        template <typename P, typename Q, typename ... Gamma, requires(AreProps<P, Q, Gamma ...>)>
+        template <typename P, typename Q, typename ... Gamma, requires(AreProps<P, Q>)>
         static auto andEL(const Formula<And<P, Q>, Assumptions<Gamma ...>> &)
         -> Formula<P, Assumptions<Gamma ...>>
         {
-            fulfill(AreProps<P, Q, Gamma ...>);
+            fulfill(AreProps<P, Q>);
             return Formula<P, Assumptions<Gamma ...>>();
         }
         
         // Gamma |- P /\ Q --> Gamma |- Q
-        template <typename P, typename Q, typename ... Gamma, requires(AreProps<P, Q, Gamma ...>)>
+        template <typename P, typename Q, typename ... Gamma, requires(AreProps<P, Q>)>
         static auto andER(const Formula<And<P, Q>, Assumptions<Gamma ...>> &)
         -> Formula<Q, Assumptions<Gamma ...>>
         {
-            fulfill(AreProps<P, Q, Gamma ...>);
+            fulfill(AreProps<P, Q>);
             return Formula<Q, Assumptions<Gamma ...>>();
         }
         
         // Gamma |- P --> Gamma |- P\/Q
-        template <typename P, typename Q, typename ... Gamma, requires(AreProps<P, Q, Gamma ...>)>
+        template <typename P, typename Q, typename ... Gamma, requires(AreProps<P, Q>)>
         static auto orIL(const Formula<P, Assumptions<Gamma ...>> &)
         -> Formula<Or<P, Q>, Assumptions<Gamma ...>>
         {
-            fulfill(AreProps<P, Q, Gamma ...>);
+            fulfill(AreProps<P, Q>);
             return Formula<Or<P, Q>, Assumptions<Gamma ...>>();
         }
         
         // Gamma |- Q --> Gamma |- P\/Q
-        template <typename P, typename Q, typename ... Gamma, requires(AreProps<P, Q, Gamma ...>)>
+        template <typename P, typename Q, typename ... Gamma, requires(AreProps<P, Q>)>
         static auto orIR(const Formula<Q, Assumptions<Gamma ...>> &)
         -> Formula<Or<P, Q>, Assumptions<Gamma ...>>
         {
-            fulfill(AreProps<P, Q, Gamma ...>);
+            fulfill(AreProps<P, Q>);
             return Formula<Or<P, Q>, Assumptions<Gamma ...>>();
         }
         
         // Gamma |- P\/Q + P, Delta |- R + Q, Lambda |- R --> Gamma, Delta, Lambda |- R
-        template <typename P, typename Q, typename R, typename ... Gamma, typename ... Delta, typename ... Lambda, requires(AreProps<P, Q, R, Gamma ..., Delta ..., Lambda ...>)>
+        template <typename P, typename Q, typename R, typename ... Gamma, typename ... Delta, typename ... Lambda, requires(AreProps<P, Q, R>)>
         static auto orE(const Formula<Or<P, Q>, Assumptions<Gamma ...>> &,
                         const Formula<R, Assumptions<P, Delta ...>> &,
                         const Formula<R, Assumptions<Q, Lambda ...>> &)
         -> Formula<R, MakeAssumptions<Gamma ..., Delta ..., Lambda ...>>
         {
-            fulfill(AreProps<P, Q, R, Gamma ..., Delta ..., Lambda ...>);
+            fulfill(AreProps<P, Q, R>);
             return Formula<R, MakeAssumptions<Gamma ..., Delta ..., Lambda ...>>();
         }
         
         // P, Gamma |- Q --> Gamma |- P=>Q
-        template <typename P, typename Q, typename ... Gamma, requires(AreProps<P, Q, Gamma ...>)>
+        template <typename P, typename Q, typename ... Gamma, requires(AreProps<P, Q>)>
         static auto impI(const Formula<Q, Assumptions<P, Gamma ...>> &)
         -> Formula<Imp<P, Q>, Assumptions<Gamma ...>>
         {
-            fulfill(AreProps<P, Q, Gamma ...>);
+            fulfill(AreProps<P, Q>);
             return Formula<Imp<P, Q>, Assumptions<Gamma ...>>();
         }
         
         // Gamma |- P => Q + Delta |- P --> Gamma, Delta |- Q
-        template <typename P, typename Q, typename ... Gamma, typename ... Delta, requires(AreProps<P, Q, Gamma ..., Delta ...>)>
+        template <typename P, typename Q, typename ... Gamma, typename ... Delta, requires(AreProps<P, Q>)>
         static auto impE(const Formula<Imp<P, Q>, Assumptions<Gamma ...>> &,
                          const Formula<P, Assumptions<Delta ...>> &)
         -> Formula<Q, MakeAssumptions<Gamma ..., Delta ...>>
         {
-            fulfill(AreProps<P, Q, Gamma ..., Delta ...>);
+            fulfill(AreProps<P, Q>);
             return Formula<Q, MakeAssumptions<Gamma ..., Delta ...>>();
         }
         
